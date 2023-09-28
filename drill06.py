c003c1e7a265a90a4510ab78999e9ed577719b3f
WonHyung Lee(Gram) <link8879@naver.com>
2023-09-28 23:02:39 +0900

    캐릭터 움직임 추가

--- drill06.py
+++ drill06.py
@@ -9,10 +9,15 @@ character = load_image('animation_sheet.png')
 hand_arrow = load_image('hand_arrow.png')
 
 running = True
-hand_x, hand_y = 0, 0
+frame = 0
 points_x = []
 points_y = []
-
+x = 100
+y = 100
+origin_x = 100
+origin_y = 100
+t = 0
+i = 0
 def handle_events():
     global running
     events = get_events()
@@ -28,10 +33,32 @@ def draw_hand():
     for i in range(0, len(points_x)):
         hand_arrow.draw(points_x[i],points_y[i])
 
+def character_move():
+    global x, y, origin_x, origin_y, t, i
+
+    if len(points_x) == 0 and len(points_y) == 0:
+        return
+
+    t = i/100
+    x = (1 - t) * origin_x + t * points_x[0]
+    y = (1 - t) * origin_y + t * points_y[0]
+
+    if x == points_x[0] and y == points_y[0]:
+        origin_x = points_x.pop(0)
+        origin_y = points_y.pop(0)
+        i = 0
+    i += 5
+
+
 while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     handle_events()
     draw_hand()
+    character_move()
+
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    frame = (frame + 1) % 8
+
     update_canvas()
     delay(0.1)
